e07d8d5806524e4225a685c612faa7495417b9ec
Thilina229 <thilinaindunil853@gmail.com>
2025-05-31 07:58:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ MONGODB: process.env.MONGODB || "mongodb://mongo:zATSetwQySOzzuYMHcxPhjWrAnoKusU
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
-};   
+};                                
                                                                      
